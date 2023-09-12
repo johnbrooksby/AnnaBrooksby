9f465390a23c9f901d4a68fb13c8afdbd64c20e8
Anna Brooksby <johnhbrooksby@yahoo.com>
2023-09-12 14:34:25 -0600

    Removing defaults and adding custom text

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,8 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
+    <div>
+      {/* <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -27,8 +27,9 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
-    </>
+      </p> */}
+      <h1>This is the beginning of Wonderful Anna's Wonderful website!</h1>
+    </div>
   )
 }
 
