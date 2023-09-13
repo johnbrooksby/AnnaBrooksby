a0e3584fd425fa5910413aafe6856160d25b1bea
Anna Brooksby <johnhbrooksby@yahoo.com>
2023-09-12 18:11:43 -0600

    Basic structure started

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Books from './Books'
+import Blog from './Blog'
+
+const Navbar = () => {
+  return (
+    <div>
+    <Books />
+    <Blog />
+    </div>
+  )
+}
+
+export default Navbar
